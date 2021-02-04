dd072a9a85c7e696c8edef3d03273bb26bb59840
sarah-svg <kjskillen@gmail.com>
2021-02-04 14:40:36 -0800

    itemHistory outline

---
+++ src/components/resty/ItemHistory.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ItemHistory = ({ method, url, id }) => {
+  return (
+    <spn>
+
+    </spn>
+
+  );
+};
+ItemHistory.propTypes = {
+
+};
+
+export default ItemHistory;
+
+
+  
