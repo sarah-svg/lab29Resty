e3c7eb6bc1c3445c6e954309ecf76e3033e719fb
sarah-svg <kjskillen@gmail.com>
2021-02-04 15:31:51 -0800

    containor body

--- src/containors/Resty.jsx
+++ src/containors/Resty.jsx
@@ -3,9 +3,16 @@ import Head from '../components/resty/Head';
 import HistoryList from '../components/resty/ListHistory';
 import Form from '../components/resty/Form';
 import Display from '../components/resty/Display';
+
+//////// Resty holds state and all the
+/////// functions display form itemHistory listHistory
 export default class Resty extends Component {
   state = {
-
+    url: '',
+    method: '',
+    body: '',
+    history: [],
+    display: {}
   }
   render() {
     return (
