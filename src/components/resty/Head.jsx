752eddabf4a1b3bc190ecc27fb704e39e78d1b31
sarah-svg <kjskillen@gmail.com>
2021-02-04 14:19:57 -0800

    compnents/resty

---
+++ src/components/resty/Head.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Head.css';
+
+export default function Head() {
+  return (
+    <div>
+      <header className={styles.Head} >
+        <h1>Resty a postman remake</h1>
+      </header>
+    </div>
+  );
+}
