053ca47278e7bc8aebd1e1bffc3203f9e9da6bc3
sarah-svg <kjskillen@gmail.com>
2021-02-05 14:45:47 -0800

    test1

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import App from './App';
 
 describe('App component', () => {
   it('renders App', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = (<App />);
     expect(wrapper).toMatchSnapshot();
   });
 });
